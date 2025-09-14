9d9fbc2c0e111864585304d2663fda4fd0554010
Ruhai_Qiu <qrh818272@gmail.com>
2025-09-14 13:45:15 +0800

    shadow mapping

--- shader/screen/screen.fs
+++ shader/screen/screen.fs
@@ -3,11 +3,20 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D screenTexture;
+uniform sampler2D depthMap;
+uniform float near_plane;
+uniform float far_plane;
+
+// required when using a perspective projection matrix
+float LinearizeDepth(float depth)
+{
+    float z = depth * 2.0 - 1.0; // Back to NDC
+    return (2.0 * near_plane * far_plane) / (far_plane + near_plane - z * (far_plane - near_plane));
+}
 
 void main()
 {
-    vec3 col = texture(screenTexture, TexCoords).rgb;
-    //float grayscale = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
-    FragColor = vec4(col, 1.0);
+    float depthValue = texture(depthMap, TexCoords).r;
+    FragColor = vec4(vec3(LinearizeDepth(depthValue) / far_plane), 1.0); // perspective
+    // FragColor = vec4(vec3(depthValue), 1.0); // orthographic
 }
